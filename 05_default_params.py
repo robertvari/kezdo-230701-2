ab2e83a883876c74bdc71a078ef9fdfe98c9e3b9
Robert Vari <mail.robertvari@gmail.com>
2023-07-15 14:51:24 +0200

    update

---
+++ 05_default_params.py
@@ -0,0 +1,11 @@
+def say_hello(name, email=None, address=None):
+    print(f"Hello {name}")
+
+    if email:
+        print(f"Your email is: {email}")
+
+    if address:
+        print(f"Your address is: {address}")
+
+
+say_hello("Csaba", "csaba@gmail.com", "Budapest")
