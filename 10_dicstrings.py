176cc397b567af72892abda2813ce4833d6b592d
Robert Vari <mail.robertvari@gmail.com>
2023-07-15 15:25:56 +0200

    update

---
+++ 10_dicstrings.py
@@ -0,0 +1,11 @@
+def print_user_data(name:str, address:str, email:str, age:int):
+    """
+    This function prints the user data in a readable format.    
+    """
+
+    print("-"*20)
+    print(f"Name: {name}")
+    print(f"Address: {address}")
+    print(f"Email: {email}")
+    print(f"Age: {age}")
+    print("-"*20)
