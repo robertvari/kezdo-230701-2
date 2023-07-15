e081848c00c44c0b53735f7fcef37e038866406a
Robert Vari <mail.robertvari@gmail.com>
2023-07-15 14:43:08 +0200

    update

---
+++ 04_keyword_params.py
@@ -0,0 +1,7 @@
+def say_hello(name, email, address):
+    print(f"Hello {name}")
+    print(f"Your email is: {email}")
+    print(f"Your address is: {address}")
+
+
+say_hello(email="csaba@gmail.com", address="Budapest", name="Csaba")
