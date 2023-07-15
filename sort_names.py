198e598d77b3172695353a708d27ab067533f2a4
Robert Vari <mail.robertvari@gmail.com>
2023-07-15 16:26:11 +0200

    update

--- sort_names.py
+++ sort_names.py
@@ -1,1 +1,11 @@
 name_list = ["Vári Róbert", "Kiss Elemér", "Nagy Adrienn", "Tóth Barna", "Kiss Elemér Aladár"]
+
+movie_list = {
+    1: {"title": "The Princess", "rating": 26},
+    2: {"title": "Spider-Man: No Way Home", "rating": 81},
+    3: {"title": "Morbius", "rating": 64},
+    4: {"title": "The Northman", "rating": 72},
+    5: {"title": "Troll", "rating": 67},
+    6: {"title": "Avatar", "rating": 76},
+    7: {"title": "The Woman King", "rating": 79}
+}
