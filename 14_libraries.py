77f9cc4ec9561595ea27e6dc1efc55e619bba148
Robert Vari <mail.robertvari@gmail.com>
2023-07-15 17:27:28 +0200

    update

---
+++ 14_libraries.py
@@ -0,0 +1,6 @@
+from my_functions.lambda_math_functions import add_numbers, divide_numbers, multiply_numbers
+# from my_functions import math_functions
+
+from my_functions.settings import *
+
+print(USERS_FOLDER, DATABASE, DATABASE_USER, DATABASE_PASSWORD)
---
+++ my_functions/lambda_math_functions.py
@@ -0,0 +1,3 @@
+add_numbers = lambda a, b: a+b
+divide_numbers = lambda a, b: a/b
+multiply_numbers = lambda a, b: a*b
