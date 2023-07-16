f92f28fcddc0b9192c093dc862680050950a27cf
Robert Vari <mail.robertvari@gmail.com>
2023-07-16 16:00:32 +0200

    update

--- magic_number.py
+++ magic_number.py
@@ -8,3 +8,31 @@ REWARD = 10
 # player variables
 CREDITS = 10
 PLAYER_NAME = None
+
+
+def main():
+    # clear screen
+    clear_screen()
+
+    # print game intro
+    intro()
+
+    # todo get player name
+    # todo start game loop
+
+def clear_screen():
+    if platform.system().lower() == "windows":
+        os.system("cls")
+    else:
+        os.system("clear")
+
+def intro():
+    print("-"*50, "MAGIC NUMBER GAME", "-"*50)
+    print(f"I have a number between {MIN} and {MAX}. Can you guess it?")
+
+def get_player_name() -> str:
+    pass
+
+
+# start program
+main()
