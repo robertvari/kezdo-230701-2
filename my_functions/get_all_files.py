b2f3ac6c1f0eef441dfcb09b72ad8b63297e7553
Robert Vari <mail.robertvari@gmail.com>
2023-07-16 14:57:42 +0200

    update

--- my_functions/get_all_files.py
+++ my_functions/get_all_files.py
@@ -13,6 +13,23 @@ def get_all_files(root_folder: str, file_list: list):
 
     # check file_list validity
     assert isinstance(file_list, list), "file_list must be an empty list"
-    assert len(file_list) == 0, "file_list must be an empty list"
 
+    # get all content in current folder
+    folder_content = [os.path.join(root_folder, i) for i in os.listdir(root_folder)]
     
+    # collect folders and files in current folder
+    subfolders = []
+    found_files = []
+    for i in folder_content:
+        if os.path.isfile(i):
+            found_files.append(i)
+        else:
+            subfolders.append(i)
+    
+    # add new files to existing file_list
+    file_list += found_files
+
+
+    # step intu subfolders and continue
+    for folder in subfolders:
+        get_all_files(folder, file_list)
